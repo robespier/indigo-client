ee8ee9197c397c5e4c5fc988b97e5ad2c839c98f
Roman Sergeev <robespier@rambler.ru>
2015-06-29 12:25:28 +0300

    Поменял NodeWebkit на Electron

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-﻿var server = require("./server");
-server.start();
+var app = require('app')
+var BrowserWindow = require('browser-window')
+
+app.on('ready', function(){
+  var mainWindow = new BrowserWindow({
+    width: 1220,
+    height: 900
+  })
+  mainWindow.loadUrl('file://' + __dirname + '/view/index.html')
+})
