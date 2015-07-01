d2a7f4793432f0b25f2c3f0983a25e5c1a7b2af1
Roman Sergeev <robespier@rambler.ru>
2015-07-01 13:01:38 +0300

    Перенес mainWindow в глобальную область видимости
    Пофиксен баг с вылетом приложения. Сборщик мусора теперь не страшен.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var app = require('app')
 var BrowserWindow = require('browser-window')
 
+var mainWindow = null;
+
 app.on('ready', function(){
-  var mainWindow = new BrowserWindow({
-    width: 1220,
-    height: 900
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    'auto-hide-menu-bar': true,
   })
-  mainWindow.loadUrl('file://' + __dirname + '/view/index.html')
+  mainWindow.loadUrl('file://' + __dirname + '/view/index.html');
+  mainWindow.focus();
 })
