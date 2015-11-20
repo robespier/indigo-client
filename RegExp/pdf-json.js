0cfa70f8d47a4d9c7b2d915f52c3bedf33bb2072
Roman Sergeev <robespier@rambler.ru>
2015-11-20 08:39:08 +0000

    Модификации вотчера
    * выходной json получает номер заказа в имя файла;
    * после парсинга исходный pdf удаляется;

--- RegExp/pdf-json.js
+++ RegExp/pdf-json.js
@@ -1,12 +1,11 @@
-var fs = require('fs'),
-  _ = require("lodash");
+var fs = require('fs');
 
 var asystem = require('a-system');
 
 var outPrefix = 'print/';
 
 /**
- * 
+ * Массив файлов, находящихся в обработке
  */
 var processing = [];
 
@@ -14,12 +13,9 @@ var processing = [];
  * Следим за папкой `print`, если там чего новое -- парсим!
  */
 fs.watch(outPrefix, function(event, filename) {
-  console.log('event is: ' + event, 'filename is:' + filename);
+  //console.log('event is: ' + event, 'filename is:' + filename);
   var out = outPrefix + filename;
   if (event === 'change' && processing.indexOf(out) === -1) {
-    /**if (filename.match(/\.raw\.json$/)) {
-      parse(out);
-    }*/
     if (filename.match(/\.pdf$/)) {
       /**
        * Ставим файл в очередь обработки, чтобы не гонять один 
@@ -31,13 +27,22 @@ fs.watch(outPrefix, function(event, filename) {
        */
       asystem.pdf2json(out)
         .then(function(data) {
-          //fs.writeFileSync(`${out}.raw.json`, JSON.stringify(data, null, '\t'));
           var result = asystem.parseJsonOrder(data);
-          var newFileName = outPrefix + filename.replace(/\.pdf$/,'.json');
-          fs.writeFileSync(newFileName, JSON.stringify(result, null, '\t'));
           
-          var pdfQueueIndex = processing.indexOf(out);
-          processing.splice(pdfQueueIndex, 1);
+          var newFileName = outPrefix;
+          if (result.order_number) {
+            newFileName += result.order_number + '.json';
+          } else {
+            newFileName += filename.replace(/\.pdf$/,'.json');
+          }
+          
+          fs.writeFile(newFileName, JSON.stringify(result, null, '\t'));
+
+          // Удалим исходный PDF, затем вычеркнем его из очереди обработки
+          fs.unlink(out, function() {
+            var pdfQueueIndex = processing.indexOf(out);
+            processing.splice(pdfQueueIndex, 1);
+          });
         })
         .catch(function(err) {
           console.error('fs watcher faced trouble: ', err);
@@ -45,36 +50,3 @@ fs.watch(outPrefix, function(event, filename) {
     }
   }
 });
-
-/**
- * @deprecated
- * 
-function parse(infile) {
-  // Считывание содержимого файла в память
-  fs.readFile(infile, function(err, data) {
-
-    // Если произошла ошибка, то мы генерируем исключение,
-    // и работа приложения завершается
-    if (err) throw err;
-
-    // data имеет тип Buffer, переводим в string
-    var text = data.toString();
-
-    // Красота!!!
-    var result = asystem.parseJsonOrder(text);
-
-    console.log(result);
-
-    /**
-     * Пишем красивешный json в файл
-     *
-    fs.writeFileSync(infile + '.json', JSON.stringify(result, null, '\t'));
-
-    /**
-     * Cloud9 не завершает процесс автоматически, поможем товарищу!
-     * 
-     * Теперь watcher, так что не надо
-     *
-    //process.exit();
-  });
-}*/
